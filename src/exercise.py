a738c99db2e1e3e6dc80a470a0d9c6bbaa0dc621
MattyC6969 <10145@bridgend.ac.uk>
2020-09-08 11:16:34 +0000

    atrtempt

--- src/exercise.py
+++ src/exercise.py
@@ -1,6 +1,6 @@
 def main():
-    #write your code below this line
-    message = input("Write a message...")
+  message = input("what it do")
+  print(message)
 
 if __name__ == '__main__':
-    main()
+   main()
